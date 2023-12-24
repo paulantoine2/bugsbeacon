5c1c95ef72229703348fd467dfc8b5ae3a128ba0
Paul ANTOINE <paul.antoine2@gmail.com>
2023-12-24 12:15:14 +0100

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "BugsBeacon",
@@ -24,6 +25,7 @@ export default function RootLayout({
         }
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
